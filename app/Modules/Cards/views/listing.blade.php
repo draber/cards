a3430b4d3bb71d8af4fca400384406c1e47602fd
Teferi <joel@taotesting.com>
2019-11-23 16:46:58 +0100

    Removed php

--- app/Modules/Cards/views/listing.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('modules::Common.views.layout')
-
-@section('title', 'Card Listing')
-
-@section('content')
-    @foreach($payload as $i => $entry)
-        @include('modules::Cards.views.card')
-    @endforeach
-@endsection
